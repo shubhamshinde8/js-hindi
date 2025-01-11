b261bfec42677228a289caf04a8cbb7c53880239
Shubham Shinde <shubhamshinde8746@gmail.com>
2025-01-11 11:37:52 +0000

    comparison in js

--- 01_basics/03_comparison.js
+++ 01_basics/03_comparison.js
@@ -1,8 +1,1 @@
-console.log("comparison ");
-
-console.log(2>1); // true
-console.log(2==2); // true
-console.log(2<1); // false
-console.log(2>=2); // true
-console.log(2<=2); // true
-console.log(2!=1); // true
+console.log("Comparison in jsvascript..");

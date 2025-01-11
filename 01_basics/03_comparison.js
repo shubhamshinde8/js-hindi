4f3aa2e12e3c14fe080ea21bbeda07cfe72efcec
Shubham Shinde <shubhamshinde8746@gmail.com>
2025-01-11 11:18:46 +0000

    comparison in js

---
+++ 01_basics/03_comparison.js
@@ -0,0 +1,8 @@
+console.log("comparison ");
+
+console.log(2>1); // true
+console.log(2==2); // true
+console.log(2<1); // false
+console.log(2>=2); // true
+console.log(2<=2); // true
+console.log(2!=1); // true

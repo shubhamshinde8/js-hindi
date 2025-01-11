8c6f3f3ffa3865896d0531ef71b04fb61c75ea46
Shubham Shinde <shubhamshinde8746@gmail.com>
2025-01-11 10:26:43 +0000

    learn about variables,constatnt and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+let accountId=987;
+var accountEmail="shubhamshinde8746@gmail.com";
+const accountPassword=980980;
+let accountState;
+
+
+
+console.table([accountEmail,accountId,accountPassword,accountState]);

5563af8972d8cdd2ef87e27f275ddc014e6037f4
shubhamshinde8 <shubhamshinde8746@gmail.com>
2025-01-17 23:29:03 -1200

    add comparison.js file

--- 01_basics/03_comparison.js
+++ 01_basics/03_comparison.js
@@ -1,1 +1,31 @@
 console.log("Comparison in jsvascript..");
+console.log("this is javascript ")
+let a=12;
+let b=45;
+let c=a+b;
+console.log("answer : "+c);
+
+const name="shubham";
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.substring(2,5));
+console.log(name.charAt(1));
+console.log(name.lastIndexOf('h'));
+
+const newName=name.substring(0,4);
+console.log(newName);
+
+const anotherName=name.slice(-6,4);
+console.log(anotherName);
+
+const mainName="   Rutik Tayde ";
+console.log(mainName);
+console.log(mainName.trim());
+const url="https:/shubham56.aurveda.com";
+
+console.log(url);
+
+console.log(url.replace('56','8746'));
+
+const ans=url.includes('shubham');
+console.log(ans);

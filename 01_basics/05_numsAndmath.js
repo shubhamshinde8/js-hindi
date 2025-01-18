aa4bf31d50fe988a557f6b857013b8eea54e00de
shubhamshinde8 <shubhamshinde8746@gmail.com>
2025-01-18 03:08:18 -1200

    save all changes

--- 01_basics/05_numsAndmath.js
+++ 01_basics/05_numsAndmath.js
@@ -1,15 +1,21 @@
-const score=340;
-console.log(score);
+// const score=340;
+// console.log(score);
 
-let balance=new Number(34);
-console.log(balance);
+// let balance=new Number(34);
+// console.log(balance);
 
-console.log(balance.toString());
-balance=balance.toString();
-console.log(balance.typeOf);
-
-
-const hundreds=1748482746;
-console.log(hundreds.toLocaleString());
+// console.log(balance.toString());
+// balance=balance.toString();
+// console.log(balance.typeOf);
 
 
+// const hundreds=1748482746;
+// console.log(hundreds.toLocaleString());
+
+
+console.log(Math.max(34,65,1,3));
+console.log(Math.min(5,2,78,43,0));
+console.log(Math.floor(1,11,2));
+console.log(Math.random());
+
+console.log(Math.max(Math.random()));
---
+++ 01_basics/06_dateAndTime.js
@@ -0,0 +1,12 @@
+console.log("Date and Time ");
+let myDate=new Date();
+console.log(myDate);
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toDateString());
+console.log(typeof myDate);
+
+
+let myTimeStamp= Date.now();
+console.log(myTimeStamp);

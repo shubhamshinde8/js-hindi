f606d4300ee23b83e2ce4eab640e431d5935ba00
Shubham Shinde <shubhamshinde8746@gmail.com>
2025-02-26 04:19:01 -1200

    save all changes

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,22 @@
+console.log("conversion in javascrip..");
+
+// let num1=33;
+// console.log(typeof num1);
+
+// let StringNumber=String(num1);
+
+// console.log(typeof StringNumber);
+
+// let as="22";
+// let sa=90;
+// let cd=87;
+// let ans=sa+as+cd;
+// console.log( ans);
+
+console.log(2+3);
+console.log(1-0);
+console.log(1*8);
+console.log(3/1);
+console.log(14%2);
+
+
